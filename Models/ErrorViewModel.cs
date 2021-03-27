abb626d6e3432d1beb81eb438e04aaf5d1faacc8
Matt Kimberley <matt.kimberley@hotmail.com>
2021-03-27 20:33:49 +0000

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotnet_hello_world.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
